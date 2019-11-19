6d550ba4cf326e5eb6cb074c30a6ffba99147015
Qwedfgr <georgiykvelidze@gmail.com>
2019-11-19 20:28:17 +0300

    добавил проверку http ответа

--- vk.py
+++ vk.py
@@ -31,16 +31,18 @@ def wall_post(auth_params, photo_id, comment):
         'message': comment
     })
     response = requests.post(url=url, params=params)
+    check_response(response)
     return response.json()
 
 
 def get_photo_id(photo_params):
-    return "photo{owner_id}_{id}".format(**photo_params)
+    return 'photo{owner_id}_{id}'.format(**photo_params)
 
 
 def get_upload_url(auth_params):
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     response = requests.get(url=url, params=auth_params)
+    check_response(response)
     return response.json()['response']['upload_url']
 
 
@@ -48,8 +50,9 @@ def upload_photo(auth_params, image_path):
     api_host = get_upload_url(auth_params)
     params = auth_params
     with open(image_path, 'rb') as file:
-        files = {"photo": file}
+        files = {'photo': file}
         response = requests.post(url=api_host, files=files, params=params)
+        check_response(response)
     return response.json()
 
 
@@ -57,4 +60,13 @@ def save_wall_photo(auth_params, upload_photo_params):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     params = dict(auth_params, **upload_photo_params)
     response = requests.post(url=url, params=params)
+    check_response(response)
     return response.json()
+
+
+def check_response(response):
+    response.raise_for_status()
+
+    json_data = response.json()
+    if 'error' in json_data:
+        raise requests.exceptions.HTTPError(json_data['error'])
